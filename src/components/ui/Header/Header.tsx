5f93d6efbc653e9be6fc235d69cc4c1f443fac92
Bohdan Shraier <bohdanshraier@Bohdans-MacBook-Air.local>
2025-01-20 13:03:02 +0200

    made header to be sticky

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -7,11 +7,13 @@ const Header: FC = () => {
 	const location = useLocation();
 	console.log(location.pathname);
 	return (
-		<header className={styles.header}>
-			<nav className={styles.header_nav}>
-				<WhiteButton active={location.pathname == '/edit'} to={'/edit'} text={'Edit users'} />
-				<WhiteButton active={location.pathname == '/users'} to={'/users'} text={'Users'} />
-			</nav>
+		<header className={styles.container}>
+			<div className={styles.header}>
+				<nav className={styles.header_nav}>
+					<WhiteButton active={location.pathname == '/edit'} to={'/edit'} text={'Edit users'} />
+					<WhiteButton active={location.pathname == '/users'} to={'/users'} text={'Users'} />
+				</nav>
+			</div>
 		</header>
 	);
 };
